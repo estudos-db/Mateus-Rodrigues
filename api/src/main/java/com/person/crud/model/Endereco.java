643c33e7c65134e387001af945bb7b43cd20a65a
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-06-23 16:06:08 -0300

    configuração de banco de dados e adição das entidas já com relacinamento

---
+++ api/src/main/java/com/person/crud/model/Endereco.java
@@ -0,0 +1,25 @@
+package com.person.crud.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String rua;
+    private int numeroCasa;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+}
